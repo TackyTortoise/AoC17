9ade6c13c0977290e27edc95fa4c49253fbfde26
Dieter Tack <dieter.tack@outlook.com>
2017-12-08 10:54:26 +0100

    Day 07 (not so clean code)

--- AoC17/AoC17/Day_07/Challenge_07.cpp
+++ AoC17/AoC17/Day_07/Challenge_07.cpp
@@ -6,13 +6,169 @@ Challenge_07::~Challenge_07(){}
 
 void Challenge_07::Run()
 {
-	P1();
+	P2();
 }
 
 void Challenge_07::P1()
 {
+	auto input = TxtFileGetLines("./Day_07/Input_07.txt");
+	vector<Program*> programs;
+	for (auto l : input)
+	{
+		regex wordRgx("[a-z]+");
+		smatch sm;
+		vector<string> names;
+		string lineCopy = l;
+		while (regex_search(lineCopy, sm, wordRgx))
+		{
+			names.push_back(sm[0]);
+			lineCopy = sm.suffix();
+		}
+		regex digitRgx("\\d+");
+		regex_search(l, sm, digitRgx);
+
+		string name = names[0];
+		names.erase(names.begin());
+		int weight = stoi(sm[0]);
+		Program* p = nullptr;
+		auto it = find_if(programs.begin(), programs.end(), [=](Program* p) {return p->name == name; });
+		if (it != programs.end())
+		{
+			auto ind = it - programs.begin();
+			programs[ind]->weight = weight;
+			programs[ind]->children = names;
+			p = programs[ind];
+		}
+		else
+		{
+			p = new Program(name, weight, names, nullptr);
+			programs.push_back(p);
+		}
+
+		for (auto n : names)
+		{
+			auto it = find_if(programs.begin(), programs.end(), [=](Program* p) {return p->name == n; });
+			if (it != programs.end())
+			{
+				//Set parent
+				auto ind = it - programs.begin();
+				programs[ind]->parent = p;
+			}
+			else
+			{
+				//Create entry for parent
+				vector<string> empty;
+				auto prog = new Program(n, 0, empty, nullptr);
+				prog->parent = p;
+				programs.push_back(prog);
+			}
+		}		
+	}
+	auto it = find_if(programs.begin(), programs.end(), [](Program* p) {return p->parent == nullptr; });
+	auto ind = it - programs.begin();
+	cout << programs[ind]->name << endl;
 }
 
 void Challenge_07::P2()
 {
+	auto input = TxtFileGetLines("./Day_07/Input_07.txt");
+	vector<Program*> programs;
+	for (auto l : input)
+	{
+		regex wordRgx("[a-z]+");
+		smatch sm;
+		vector<string> names;
+		string lineCopy = l;
+		while (regex_search(lineCopy, sm, wordRgx))
+		{
+			names.push_back(sm[0]);
+			lineCopy = sm.suffix();
+		}
+		regex digitRgx("\\d+");
+		regex_search(l, sm, digitRgx);
+
+		string name = names[0];
+		names.erase(names.begin());
+		int weight = stoi(sm[0]);
+		Program* p = nullptr;
+		auto it = find_if(programs.begin(), programs.end(), [=](Program* p) {return p->name == name; });
+		if (it != programs.end())
+		{
+			auto ind = it - programs.begin();
+			programs[ind]->weight = weight;
+			programs[ind]->children = names;
+			p = programs[ind];
+		}
+		else
+		{
+			p = new Program(name, weight, names, nullptr);
+			programs.push_back(p);
+		}
+
+		for (auto n : names)
+		{
+			auto it = find_if(programs.begin(), programs.end(), [=](Program* p) {return p->name == n; });
+			if (it != programs.end())
+			{
+				//Set parent
+				auto ind = it - programs.begin();
+				programs[ind]->parent = p;
+			}
+			else
+			{
+				//Create entry for parent
+				vector<string> empty;
+				auto prog = new Program(n, 0, empty, nullptr);
+				prog->parent = p;
+				programs.push_back(prog);
+			}
+		}
+	}
+	auto it = find_if(programs.begin(), programs.end(), [](Program* p) {return p->parent == nullptr; });
+	auto ind = it - programs.begin();
+	int save1 = -1;
+	int save2 = -1;
+	Program* p1 = nullptr;
+	Program* p2 = nullptr;
+	for (int i = 0; i < programs[ind]->children.size(); ++i)
+	{
+		auto c = programs[ind]->children[i];
+		auto itc = find_if(programs.begin(), programs.end(), [=](Program* p) {return p->name == c; });
+		if (itc != programs.end())
+		{
+			auto indc = itc - programs.begin();
+			int w = programs[indc]->GetChildrenWeight(programs);
+			if (w != save1)
+			{
+				save1 = w;
+				p1 = programs[indc];
+				if (save2 < 0)
+				{
+					save2 = w;
+					p2 = programs[indc];
+				}
+			}
+			if (save1 != save2)
+			{
+				//cout << "Weight difference: " + to_string(abs(save1 - save2)) << endl;
+				auto  itNextName = find_if(programs[ind]->children.begin(), programs[ind]->children.end(), [=](string s) {return s != p1->name && s != p2->name; });
+				auto indNextName = itNextName - programs[ind]->children.begin();
+				auto itn = find_if(programs.begin(), programs.end(), [=](Program* p) {return p->name == programs[ind]->children[indNextName]; });
+				if (itn != programs.end())
+				{
+					auto indn = itn - programs.begin();
+					Program* next = programs[indn];
+					auto nextWeight = next->GetChildrenWeight(programs);
+
+					if (save1 != nextWeight)
+						p1->GetChildrenWeight(programs, true, save2, abs(save2 - save1));
+					else
+						p2->GetChildrenWeight(programs, true, save1, abs(save2 - save1));
+					i = -1;
+					break;
+				}
+			}
+		}
+	}
+	cout << programs[ind]->name << endl;
 }
--- AoC17/AoC17/Day_07/Challenge_07.h
+++ AoC17/AoC17/Day_07/Challenge_07.h
@@ -10,5 +10,103 @@ public:
 protected:
 	void P1() override;
 	void P2() override;
+
+private:
+	struct Program
+	{
+		Program(string n, int w, const vector<string> c, Program* p) : name(n), weight(w), children(c), parent(p) {}
+		string name;
+		int weight;
+		vector<string> children;
+		Program* parent;
+		int GetChildrenWeight(const vector<Program*> programs, bool findDifference = false, int otherw = 0, int diffToFind = 0)
+		{
+			int total = 0;
+			for (auto c : children)
+			{
+				auto it = find_if(programs.begin(), programs.end(), [=](Program* p) {return p->name == c; });
+				if (it != programs.end())
+				{
+					auto ind = it - programs.begin();
+					total += programs[ind]->GetChildrenWeight(programs);
+				}
+			}
+			if (findDifference)
+			{
+				if (total + weight != otherw)
+				{
+					auto o = GetOddInChildren(this, programs, otherw);
+					auto last = o;
+					while (o != nullptr)
+					{
+						last = o;
+						o = GetOddInChildren(o, programs, otherw);
+					}
+
+					for (auto child : last->children)
+					{
+						auto prog = GetProgramByName(child, programs);
+						cout << prog->GetChildrenWeight(programs) << endl;
+					}
+					cout << "should be: " << last->weight - diffToFind << endl;
+				}
+			}
+
+			return total + weight;
+		}
+
+		Program* GetProgramByName(string name, const vector<Program*>& programs)
+		{
+			return programs[find_if(programs.begin(), programs.end(), [=](Program* p) {return p->name == name; }) - programs.begin()];
+		}
+
+		vector<Program*> NameToProgramChildren(const vector<string>& names, const vector<Program*>& programs)
+		{
+			vector<Program*> result;
+			for(auto n : names)
+			{
+				result.push_back(GetProgramByName(n, programs));
+			}
+			return result;
+		}
+
+		Program* GetOddInChildren(Program* p, const vector<Program*>& programs, const int otherw)
+		{
+			auto childPrograms = NameToProgramChildren(p->children, programs);
+			int weight1 = -1;
+			int weight2 = -1;
+			Program* p1 = nullptr;
+			Program* p2 = nullptr;
+
+			for (auto c : childPrograms)
+			{
+				if (c->GetChildrenWeight(programs)!= weight1)
+				{
+					weight1 = c->GetChildrenWeight(programs);
+					p1 = c;
+					if (weight2 < 0)
+					{
+						weight2 = c->GetChildrenWeight(programs);
+						p2 = c;
+					}
+				}
+				if (weight1 != weight2)
+				{
+					int w1c = count_if(childPrograms.begin(), childPrograms.end(), [=](Program* pr) {return pr->GetChildrenWeight(programs) == weight1; });
+					int w2c = count_if(childPrograms.begin(), childPrograms.end(), [=](Program* pr) {return pr->GetChildrenWeight(programs) == weight2; });
+					if (w1c < w2c)
+					{
+						return p1;
+					}
+					else
+					{
+						return p2;
+					}
+				}
+			}
+
+			return nullptr;
+		}
+	};
 };
 
